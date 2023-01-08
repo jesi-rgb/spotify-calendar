034fec621d6edda4021e86dbc566ed14821b7bfb
Jesús Enrique Rascón <blograso@gmail.com>
2023-01-08 13:16:02 +0100

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="font-body mx-auto max-w-sm xl:max-w-xl 2xl:max-w-8xl my-10">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Calendar from '@event-calendar/core';
+	import TimeGrid from '@event-calendar/time-grid';
+
+	let plugins = [TimeGrid];
+	let options = {
+		allDaySlot: false,
+		view: 'timeGridWeek',
+		pointer: true,
+		editable: true,
+		eventDurationEditable: true,
+		nowIndicator: true,
+		eventMouseEnter: function (info) {
+			console.log(info);
+		},
+		// slotMinTime: new Date(Date.parse(new Date()) - 60 * 1000 * 50),
+		// slotMaxTime: new Date(Date.parse(new Date()) + 60 * 1000 * 50),
+		// slotDuration: '00:10',
+		events: [
+			// your list of events
+			{
+				start: new Date(),
+				end: new Date(Date.parse(new Date()) + 60 * 1000 * 30),
+				title: 'Mierdón'
+			}
+		]
+	};
+	function updateOptions() {
+		options.slotDuration = '00:10';
+	}
+</script>
+
+<button on:click={updateOptions}>Change slot duration</button>
+<Calendar {plugins} {options} />
