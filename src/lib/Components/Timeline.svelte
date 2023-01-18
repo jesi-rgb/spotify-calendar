8ef21a0c32eba073cf25f38bccf52c5671bbeb71
Jesús Enrique Rascón <blograso@gmail.com>
2023-01-18 16:42:59 +0100

    damn this is working!

--- src/lib/Components/Timeline.svelte
+++ src/lib/Components/Timeline.svelte
@@ -5,12 +5,16 @@
 	import { axisLeft } from 'd3-axis';
 	import { select } from 'd3-selection';
 	import { extent, max } from 'd3';
+	import { zoom } from 'd3-zoom';
+
+	export let timeResolution = 5;
+	export let my = 20;
+	export let mx = 40;
 
 	let pinYAxis;
-	let my = 20; // declare initial values for margin and svg_height/width
-	let mx = 40; // declare initial values for margin and svg_height/width
 	let svg_height = 3000;
 	let svg_width = 600;
+
 	$: height = svg_height - my * 2;
 	$: width = svg_width - mx * 3;
 
@@ -19,8 +23,19 @@
 		.range([0, height])
 		.nice();
 
+	$: yAxis = axisLeft(y).ticks(timeMinute.every(timeResolution));
+
+	const zoomBehaviour = zoom().scaleExtent([1, 30]).filter(filter).on('zoom', zoomed);
+
+	function filter(event) {
+		return (!event.ctrlKey || event.type === 'wheel') && !event.button;
+	}
+	function zoomed({ transform }) {
+		select(pinYAxis).call(yAxis.scale(transform.rescaleY(y)));
+	}
+
 	$: if (pinYAxis) {
-		select(pinYAxis).call(axisLeft(y).ticks(timeMinute.every(5)));
+		select(pinYAxis).call(yAxis).call(zoomBehaviour.on('zoom', zoomed));
 	}
 </script>
 
